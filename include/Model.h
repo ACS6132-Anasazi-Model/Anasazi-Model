86a24d47db2ca2ccde5a8b655affc26557cb596f
Yadav Dosieah <dosyad12@gmail.com>
2019-11-04 15:34:54 +0000

    Added attributes and Methods to Model.h

---
+++ include/Model.h
@@ -0,0 +1,35 @@
+#ifndef MODEL
+#define MODEL
+
+#include <boost/mpi.hpp>
+#include "repast_hpc/Schedule.h"
+#include "repast_hpc/Properties.h"
+#include "repast_hpc/SharedContext.h"
+#include "repast_hpc/SharedDiscreteSpace.h"
+#include "repast_hpc/GridComponents.h"
+
+#include "Household.h"
+
+class AnasaziModel{
+private:
+  int stopAt;
+	int boardSize;
+  std::vector<Household> listOfHousehold;
+
+	repast::Properties* props;
+	repast::SharedContext<Agent> context;
+  //Need to confirm this line
+	repast::SharedDiscreteSpace<Agent, repast::StrictBorders, repast::SimpleAdder<Agent> >* discreteSpace;
+
+public:
+	AnasaziModel(std::string propsFile, int argc, char** argv, boost::mpi::communicator* comm);
+	~AnasaziModel();
+	void initAgents();
+	void initSchedule(repast::ScheduleRunner& runner);
+	void doPerTick();
+  bool fissionHousehold();
+  bool removeHousehold();
+  int countHousehold();
+};
+
+#endif
