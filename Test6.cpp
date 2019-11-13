78516fa1366e0bcd57c5ec23f584e138ed44de9c
dmills15 <57175525+dmills15@users.noreply.github.com>
2019-11-13 10:35:43 +0000

    Add files via upload

---
+++ Test6.cpp
@@ -0,0 +1,20 @@
+#include "Model.cpp"
+
+void Test6::getHousehold(){
+	int houseNumber[sizeof(model.listOfHousehold),2] = {};
+	for (int i = 0; i < sizeof(model.listOfHousehold); i++){
+		houseNumber[i][0] = i;
+		houseNumber[i][1] = Model.listOfHousehold[i];
+	}
+
+	std:ofstream out("test6.csv");
+
+	for (int i = 0; i < sizeof(model.listOfHousehold); i++){
+		outfile << houseNumber[i][0] << ", " << houseNumber[i][1];
+		outfile << endl;
+
+	}
+	
+	outfile.close();	
+
+}
