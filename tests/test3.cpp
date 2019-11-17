ad7be18d2f064e08e6471a96396dad4698beaa7e
dmills15 <57175525+dmills15@users.noreply.github.com>
2019-11-17 22:33:52 +0000

    Added read hydro, UNTESTED

--- tests/test3.cpp
+++ tests/test3.cpp
@@ -1,9 +1,40 @@
+//Read archaeological data as input and propagate this to the network
+
 #include <stdio.h>
 #include <vector>
 #include <boost/mpi.hpp>
 
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+#include "hydro.csv"
+#include "map.csv"
+#include "pdsi.csv"
+#include "settlement.csv"
+//#include "useful data.xlsx"
+#include "water.csv"
 
 
 int main(){
-
+	
+	using namespace std;
+	const int rows = 552;
+	const int cols = 7;
+	
+	int h[552][7] = {};
+	
+	ifstream file("hydro.csv");
+	
+	if (file.is_open()){
+		float r[rows][cols];
+		
+		for (int i = 0; i < rows; i++){
+			for (int j = 0; j < cols; j++){
+				file >> r[i][j];
+				file.get();
+				h[i][j] = r[i][j];
+			}
+		}
+	}	
 }
