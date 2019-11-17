4ef0a1796384df3b18350fd9a79d3fbd114cf142
dmills15 <57175525+dmills15@users.noreply.github.com>
2019-11-17 21:40:39 +0000

    Update test2.cpp

--- tests/test2.cpp
+++ tests/test2.cpp
@@ -1,9 +1,16 @@
+//Instantiate the network and update its properties
+
 #include <stdio.h>
 #include <vector>
+#include "repast_hpc/RepastProcess.h"
 #include <boost/mpi.hpp>
 
+#include "Model.h"
+#include "Location.h"
+#include "Household.h"
+
+int main(int argc, char** argv){
 
 
-int main(){
-
+	
 }
