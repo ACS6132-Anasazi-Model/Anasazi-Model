36f4ceff98a338b3f2f9721b3351e595fd0f4bb6
myName <ryan.lo.0814@gmail.com>
2019-11-18 15:42:45 +0000

    Version 1.1 of test5.cpp

--- tests/test5.cpp
+++ tests/test5.cpp
@@ -6,10 +6,7 @@
 
 
 int main(){
-	max.age=30;
-	cout<<"max.age = "<<max.age<<endl;
-	max.age=25;
-	cout<<"max.age (smaller) = "<<max.age<<endl;
-	max.age=35;
-	cout<<"max.age (larger) = "<<max.age<<endl;
+	// 1.Directly change the parameter max.age in model.props
+	// 2.Run the model in terminal by the following code:
+	//   mpirun bin/main.exe props/config.props props/model.props
 }
