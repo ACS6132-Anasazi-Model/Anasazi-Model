0e49d23d3bbcab0b8a37fa83eef70d50e574b48a
myName <ryan.lo.0814@gmail.com>
2019-11-18 15:33:45 +0000

    Version 1.0 of test5.cpp

--- tests/test5.cpp
+++ tests/test5.cpp
@@ -2,8 +2,14 @@
 #include <vector>
 #include <boost/mpi.hpp>
 
+#include "model.props"
 
 
 int main(){
-
+	max.age=30;
+	cout<<"max.age = "<<max.age<<endl;
+	max.age=25;
+	cout<<"max.age (smaller) = "<<max.age<<endl;
+	max.age=35;
+	cout<<"max.age (larger) = "<<max.age<<endl;
 }
