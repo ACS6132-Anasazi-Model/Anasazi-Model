994049008cb769405224f9cf6068b4a2f02ee600
dmills15 <57175525+dmills15@users.noreply.github.com>
2019-11-18 15:45:55 +0000

    Update test5.cpp

--- tests/test5.cpp
+++ tests/test5.cpp
@@ -9,4 +9,5 @@ int main(){
 	// 1.Directly change the parameter max.age in model.props
 	// 2.Run the model in terminal by the following code:
 	//   mpirun bin/main.exe props/config.props props/model.props
+	// print number of agents at end of doPerTick, to show it decreasing
 }
